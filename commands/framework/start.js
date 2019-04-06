b79d1ffcf45cc4e702b34af2bc85512391ba5e86
LPIX-11 <mohamedjohnson31@gmail.com>
2019-04-06 00:14:09 +0000

    Integration of database configurations without real implementation

--- commands/framework/start.js
+++ commands/framework/start.js
@@ -27,22 +27,110 @@ class Start extends Command {
 
 	async handle({ projectName }, { blank, minimal, noInteraction }) {		
 		// No interaction from user required in this Section
-		if (`${ noInteraction }`) {
+		if (`${ noInteraction }`  === true) {
 			// Assinging a default name to the project
 			projectName = 'SamaneProject'
 
-			console.log(`\nStarting sm start -p ${projectName} with default settings ~ No Interaction Selected`)
+			console.log(`\nStarting sm start -p ${ projectName } with default settings ~ No Interaction Selected`)
 			// Calling the the default settings project downloader
-			await this.waitForDownload(`${projectName}`)
+			await this.waitForDownload(`${ projectName }`)
 		}
 		// In this section we interact with the user
-		else if (`${projectName}` === '...') {
+		else if (`${ projectName }` === '...') {
 			// Waiting for him to give the name of the project
 			projectName = await this.ask('Name of the project? ', 'SamaneProject')
 			
-			/*if (!blank && !minimal) {
-				confirmDbLink = await this.confirm('Do you want to link your project to a database? ')
-			}*/
+			if (!blank && !minimal) {
+				// Asking for connection with database if not a blank or a minimal project
+				const confirmDbLink = await this.confirm('Do you want to link your project to a database? ', { default: false })
+
+				// If the user wants connection with a database
+				// Asking what type of connection is it
+				// On wich database server
+				if (confirmDbLink) {
+					// Including the cloud databases
+					const databaseType = await this.choice('How to interact with database? ', [
+						'Relational'
+					],
+					'Relational')
+
+					/* To implement after
+						,
+						'NoSQL',
+						'Cloud'
+					*/
+
+					// Database Instace
+					let database
+
+					// Database Listening Port
+					let databasePort
+
+					// Asking for the database
+					if(databaseType === 'Relational') {
+						database = await this.choice('Which database? ', [
+							'MySQL',
+							'MariaDB',
+							'Oracle',
+							'PostgreSQL'							
+						],
+						'MySQL') // Default Selection
+
+						// Retrieve the database connection port following the selected database
+						if (database === 'MySQL') {
+							databasePort = await this.ask('Port: ', '3306')
+						
+						} else if (database === 'MariaDB') {
+							databasePort = await this.ask('Port: ', '3306')
+						
+						} else if (database === 'Oracle') {
+							databasePort = await this.ask('Port: ', '1521')
+						
+						} else {
+							databasePort = await this.ask('Port: ', '5432')
+						}
+
+
+					} else if(databaseType === 'NoSQL') {
+						database = await this.choice('Which database? ', [
+							'MongoDB',
+							'PostgreSQL'
+							
+						],
+						'MongoDB') // Default Selection
+					} else {
+						database = await this.choice('Which database? ', [
+							'Firebase',
+							'MongoDB Cloud',
+							'Oracle Cloud'
+							
+						],
+						'Firebase') // Default Selection
+					}
+
+					// Connection to the database
+					const connectionType = await this.choice('How to interact with database? ', [
+						'PDO',
+						'ORM'
+					],
+					'PDO') // Default Selection
+
+					// Retrieve the database name
+					const databaseName = await this.ask('Database Name? ', 'samane')
+
+					// Retrieve database host
+					const databaseHost = await this.ask('Database Host? ', 'localhost')
+
+					console.log('\nDatabase Configurations\n')
+
+					console.log(`Database Type: ${ databaseType }`)
+					console.log(`Database Instance: ${ database }`)
+					console.log(`Database Name: ${ databaseName }`)
+					console.log(`Database Port: ${ databasePort }`)
+					console.log(`Database Host: ${ databaseHost }`)
+					console.log(`Connection Type: ${ connectionType }`)
+				}
+			}
 
 			console.log(`\nStarting sm start -p ${projectName} ${blank ? 'with default settings' : ''} ${minimal ? 'with minimal settings' : ''}`)
 			// Calling the the default settings project downloader
